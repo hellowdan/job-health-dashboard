be3174f60dc9e41c46163a9587440718602194bc
drosa <drosa@redhat.com>
2020-08-05 23:42:13 +0200

    fixes

--- jobs-monitoring-health-middle/src/main/java/org/jboss/qa/monitoring/health/application/HealthDashboardMiddleApplication.java
+++ jobs-monitoring-health-middle/src/main/java/org/jboss/qa/monitoring/health/application/HealthDashboardMiddleApplication.java
@@ -1,12 +1,15 @@
 package org.jboss.qa.monitoring.health.application;
 
+import org.jboss.qa.monitoring.health.dao.BenchmarksRepository;
+import org.jboss.qa.monitoring.health.dao.JobsRepository;
+import org.jboss.qa.monitoring.health.dao.StatusRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"org.jboss.qa.monitoring.health"})
-@EnableJpaRepositories(basePackages= {"org.jboss.qa.monitoring.health.*"})
+@EnableJpaRepositories(basePackageClasses= {JobsRepository.class, StatusRepository.class, BenchmarksRepository.class})
 @EntityScan("org.jboss.qa.monitoring.health.*")
 public class HealthDashboardMiddleApplication {
     public static void main(String[] args) {
