9d767e3f131b034d9e58ec88c58e63e4a1e8851c
drosa <drosa@redhat.com>
2020-08-05 23:21:05 +0200

    fixes

--- jobs-monitoring-health-middle/src/main/java/org/jboss/qa/monitoring/health/application/HealthDashboardMiddleApplication.java
+++ jobs-monitoring-health-middle/src/main/java/org/jboss/qa/monitoring/health/application/HealthDashboardMiddleApplication.java
@@ -1,21 +1,15 @@
 package org.jboss.qa.monitoring.health.application;
 
-import org.jboss.qa.monitoring.health.controller.BenchmarksController;
-import org.jboss.qa.monitoring.health.controller.JobsController;
-import org.jboss.qa.monitoring.health.controller.UpdateStatusController;
-import org.jboss.qa.monitoring.health.dao.BenchmarksRepository;
-import org.jboss.qa.monitoring.health.dao.StatusRepository;
-import org.jboss.qa.monitoring.health.dao.JobsRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"org.jboss.qa.monitoring.health"})
-@EnableJpaRepositories(basePackageClasses= {JobsRepository.class, StatusRepository.class, BenchmarksRepository.class})
-@ComponentScan(basePackageClasses = {JobsController.class, UpdateStatusController.class, BenchmarksController.class})
+@SpringBootApplication(scanBasePackages={"org.jboss.qa.monitoring.health"})
+@EnableJpaRepositories(basePackages= {"org.jboss.qa.monitoring.health.*"})
 @EntityScan("org.jboss.qa.monitoring.health.*")
+@EnableAutoConfiguration
 public class HealthDashboardMiddleApplication {
     public static void main(String[] args) {
         SpringApplication.run(HealthDashboardMiddleApplication.class, args);
